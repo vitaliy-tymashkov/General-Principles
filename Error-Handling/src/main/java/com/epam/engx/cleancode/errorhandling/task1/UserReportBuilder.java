91c15ce6e0edbe3ba37ede126670d789434e0b51
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2021-03-14 21:34:28 +0100

    Task 3 (Error-Handling) - Moderate refactoring, methods extracted

--- Error-Handling/src/main/java/com/epam/engx/cleancode/errorhandling/task1/UserReportBuilder.java
+++ Error-Handling/src/main/java/com/epam/engx/cleancode/errorhandling/task1/UserReportBuilder.java
@@ -12,25 +12,39 @@ public class UserReportBuilder {
 
     private UserDao userDao;
 
-    public Double getUserTotalOrderAmount(String userId) throws IllegalArgumentException {
+    public Double getUserTotalOrderAmount(String userId) {
+        checkDaoConnection();
+        User user = checkUser(userId);
+        List<Order> orders = checkUserOrders(user);
 
+        return calculateTotal(orders);
+    }
+
+    private void checkDaoConnection() {
         if (userDao == null) {
             throw new DatabaseConnectionException();
         }
+    }
 
+    private User checkUser(String userId) {
         User user = userDao.getUser(userId);
         if (user == null) {
             throw new InvalidUserException(INVALID_USER);
         }
+        return user;
+    }
 
+    private List<Order> checkUserOrders(User user) {
         List<Order> orders = user.getAllOrders();
         if (orders.isEmpty()) {
             throw new InvalidUserException(EMPTY_ORDERS);
         }
+        return orders;
+    }
 
+    private Double calculateTotal(List<Order> orders) {
         Double sum = 0.0D;
         for (Order order : orders) {
-
             if (order.isSubmitted()) {
                 Double total = order.total();
                 if (total < 0) {
@@ -39,7 +53,6 @@ public class UserReportBuilder {
                 sum += total;
             }
         }
-
         return sum;
     }
 
