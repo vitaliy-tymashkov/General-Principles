0aaa92871e067535054d76c455acaf1e18690574
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2021-03-14 15:30:04 +0100

    Task 1 (Comments) - Test throws exception updated

--- Comments/src/test/java/com/epam/engx/cleancode/comments/task1/MortgageInstallmentCalculatorUtilityTest.java
+++ Comments/src/test/java/com/epam/engx/cleancode/comments/task1/MortgageInstallmentCalculatorUtilityTest.java
@@ -6,41 +6,41 @@ import org.junit.Test;
 
 public class MortgageInstallmentCalculatorUtilityTest {
     @Test
-    public void shouldCalculateMonthlyPaymentWhenAmountsAreSmall() throws Exception {
+    public void shouldCalculateMonthlyPaymentWhenAmountsAreSmall() throws InvalidInputException {
         double monthlyPaymentAmount = MortgageInstallmentCalculatorUtility.calculateMonthlyPayment(1000, 1, 12);
         Assert.assertEquals(88.84d, monthlyPaymentAmount, 0.01d);
     }
 
     @Test
-    public void shouldCalculateMonthlyPaymentWhenAmountIsLarge() throws Exception {
+    public void shouldCalculateMonthlyPaymentWhenAmountIsLarge() throws InvalidInputException {
         double monthlyPaymentAmount = MortgageInstallmentCalculatorUtility.calculateMonthlyPayment(10000000, 1, 12);
         Assert.assertEquals(888487.88d, monthlyPaymentAmount, 0.01d);
     }
 
     @Test
-    public void shouldCalculateMonthlyPaymentWhenPrincipalIsZero() throws Exception {
+    public void shouldCalculateMonthlyPaymentWhenPrincipalIsZero() throws InvalidInputException {
         double monthlyPaymentAmount = MortgageInstallmentCalculatorUtility.calculateMonthlyPayment(0, 1, 12);
         Assert.assertEquals(0, monthlyPaymentAmount, 0.01d);
     }
 
     @Test
-    public void shouldCalculateMonthlyPaymentWhenInterestRateIsZero() throws Exception {
+    public void shouldCalculateMonthlyPaymentWhenInterestRateIsZero() throws InvalidInputException {
         double monthlyPaymentAmount = MortgageInstallmentCalculatorUtility.calculateMonthlyPayment(1000, 1, 0);
         Assert.assertEquals(83.33, monthlyPaymentAmount, 0.01d);
     }
 
     @Test(expected = InvalidInputException.class)
-    public void shouldThrowInvalidInputExceptionOnNegativeTenure() throws Exception {
+    public void shouldThrowInvalidInputExceptionOnNegativeTenure() throws InvalidInputException {
         MortgageInstallmentCalculatorUtility.calculateMonthlyPayment(20, -10, 14.5);
     }
 
     @Test(expected = InvalidInputException.class)
-    public void shouldThrowInvalidInputExceptionOnNegativeInterestRate() throws Exception {
+    public void shouldThrowInvalidInputExceptionOnNegativeInterestRate() throws InvalidInputException {
         MortgageInstallmentCalculatorUtility.calculateMonthlyPayment(20, 1, -12);
     }
 
     @Test(expected = InvalidInputException.class)
-    public void shouldThrowInvalidInputExceptionOnNegativePrincipalAmount() throws Exception {
+    public void shouldThrowInvalidInputExceptionOnNegativePrincipalAmount() throws InvalidInputException {
         MortgageInstallmentCalculatorUtility.calculateMonthlyPayment(-20, 10, 14.5);
     }
 }
