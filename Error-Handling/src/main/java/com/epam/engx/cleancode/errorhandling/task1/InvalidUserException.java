1c45f0cba327f596ca24abf631dba053152c92ff
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2021-03-15 14:30:25 +0100

    Task 3 (Error Handling) - CR - Exceptions refactoring, methods removed.

--- Error-Handling/src/main/java/com/epam/engx/cleancode/errorhandling/task1/InvalidUserException.java
+++ Error-Handling/src/main/java/com/epam/engx/cleancode/errorhandling/task1/InvalidUserException.java
@@ -2,8 +2,7 @@ package com.epam.engx.cleancode.errorhandling.task1;
 
 public class InvalidUserException extends UserException {
 
-    public InvalidUserException(ERROR_ENUM errorEnum) {
-        super.setErrorEnum(errorEnum);
-        super.setDescription(errorEnum.getDescription());
+    protected InvalidUserException(ERROR_ENUM errorEnum) {
+        super(errorEnum);
     }
 }
--- Error-Handling/src/main/java/com/epam/engx/cleancode/errorhandling/task1/UserException.java
+++ Error-Handling/src/main/java/com/epam/engx/cleancode/errorhandling/task1/UserException.java
@@ -4,19 +4,12 @@ public abstract class UserException extends RuntimeException {
     private ERROR_ENUM errorEnum;
     private String description;
 
+    public UserException(ERROR_ENUM errorEnum) {
+        this.errorEnum = errorEnum;
+        this.description = errorEnum.getDescription();
+    }
+
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public ERROR_ENUM getErrorEnum() {
-        return errorEnum;
-    }
-
-    public void setErrorEnum(ERROR_ENUM errorEnum) {
-        this.errorEnum = errorEnum;
     }
 }
